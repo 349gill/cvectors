196ba10d36a088fb9b1bf7e9a4acf8ffc7b30bda
Harsh Gill <harsh.gill@yahoo.com>
2024-07-07 23:31:09 -0600

    Changed Multiplication Algorithm

--- src/cvectors.h
+++ src/cvectors.h
@@ -33,6 +33,7 @@ Vector* transform_vector(Matrix* m, Vector* v);
 Vector* scale_vector(float c, Vector* v);
 float magnitude_vector(Vector* v);
 Vector* normalize_vector(Vector* v);
+Vector* reflection_vector(Vector* i, Vector* n);
 
 Matrix* create_matrix(int dimension);
 void free_matrix(Matrix* m);
@@ -41,19 +42,22 @@ Matrix* subtract_matrices(Matrix* m1, Matrix* m2);
 Matrix* multiply_matrices(Matrix* m1, Matrix* m2);
 Matrix* transpose_matrix(Matrix* m);
 Matrix* scale_matrix(float c, Matrix* m);
+Matrix* scale_matrix_anisotropic(float x, float y, float z, Matrix* m);
 float determinant_matrix(Matrix* m);
+Matrix* translate_matrix(Matrix* m, float x, float y, float z);
+Matrix* translate_matrix_in_place(Matrix* m, float x, float y, float z); // TO DO
+Matrix* outer_product(Vector* v1, Vector* v2);
 
-// Matrix Helper Functions
-Matrix* strassen(Matrix* A, Matrix* B, int dim);
-Matrix* pad_matrix(Matrix* m, int new_dim);
-Matrix* unpad_matrix(Matrix* m, int original_dim);
-Matrix* determinant_helper(Matrix* m, int exclude_row, int exclude_col);
-
+// TO DO
+Matrix* rotate_matrix(Matrix* m, float x, float y, float z, float angle);
+Matrix* rotate_matrix_X(Matrix* m, float angle);
+Matrix* rotate_matrix_Y(Matrix* m, float angle);
+Matrix* rotate_matrix_Z(Matrix* m, float angle);
 
 // Rotating matrices, Translating matrices, Inverting matrices, Orthonormalization, Eigen stuff
 // Gram–Schmidt process for orthogonal matrices, Vector projections, Frustum projections, Quaternions
 // Matrix raised to some power. Sine, Cosine, and Tangent of a Matrix
-// Maximum and Minimum vectors, reflected vectors
+// Maximum and Minimum vectors, Identity Matrix, Duplicate Vector/Matrix;
 
 Vector* add_vectors(Vector* v1, Vector* v2) {
     if (v1->dimension != v2->dimension)
@@ -95,16 +99,27 @@ float dot_product(Vector* v1, Vector* v2) {
 }
 
 Vector* cross_product(Vector* v1, Vector* v2) {
-    if (v1->dimension != 3 || v2->dimension != 3)
-        return NULL;
+    if (v1->dimension == 3 && v2->dimension == 3) {
+        Vector* v = (Vector*) malloc(sizeof(Vector));
+        v->dimension = v1->dimension;
+        v->elements = (float *) malloc(v->dimension * sizeof(float));
+        v->elements[0] = (v1->elements[1] * v2->elements[2]) - (v1->elements[2] * v2->elements[1]);
+        v->elements[1] =  - ((v1->elements[0] * v2->elements[2]) - (v1->elements[2] * v2->elements[0]));
+        v->elements[2] =  (v1->elements[0] * v2->elements[1]) - (v1->elements[1] * v2->elements[0]);
+        return v;
+    }
+    if ((v1->dimension == 4 && v2->dimension == 4)) {
+        Vector* v = (Vector*) malloc(sizeof(Vector));
+        v->dimension = v1->dimension;
+        v->elements = (float *) malloc(v->dimension * sizeof(float));
+        v->elements[0] = (v1->elements[1] * v2->elements[2]) - (v1->elements[2] * v2->elements[1]);
+        v->elements[1] =  - ((v1->elements[0] * v2->elements[2]) - (v1->elements[2] * v2->elements[0]));
+        v->elements[2] =  (v1->elements[0] * v2->elements[1]) - (v1->elements[1] * v2->elements[0]);
+        v->elements[3] = 1.f;
+        return v;
+    }
 
-    Vector* v = (Vector*) malloc(sizeof(Vector));
-    v->dimension = v1->dimension;
-    v->elements = (float *) malloc(v->dimension * sizeof(float));
-    v->elements[0] = (v1->elements[1] * v2->elements[2]) - (v1->elements[2] * v2->elements[1]);
-    v->elements[1] =  - ((v1->elements[0] * v2->elements[2]) - (v1->elements[2] * v2->elements[0]));
-    v->elements[2] =  (v1->elements[0] * v2->elements[1]) - (v1->elements[1] * v2->elements[0]);
-    return v;
+    return NULL;
 }
 
 
@@ -158,6 +173,20 @@ Vector* normalize_vector(Vector* v) {
     return n;
 }
 
+Vector* reflection_vector(Vector* i, Vector* n) {
+    Vector* r = (Vector*) malloc(sizeof(Vector));
+    r->dimension = i->dimension;
+    r->elements = (float *) malloc(r->dimension * sizeof(float));
+
+    Vector* temp = scale_vector(2.f * dot_product(i, n), n);
+    r = subtract_vectors(i, temp);
+
+    free(temp->elements);
+    free(temp);
+
+    return r;
+}
+
 Matrix* create_matrix(int dimension) {
     Matrix* m = (Matrix*) malloc(sizeof(Matrix));
     m->dimension = dimension;
@@ -196,108 +225,19 @@ Matrix* subtract_matrices(Matrix* m1, Matrix* m2) {
     return m;
 }
 
-// Strassen’s Matrix Multiplication
-Matrix* pad_matrix(Matrix* m, int new_dim) {
-    Matrix* padded = create_matrix(new_dim);
-    for (int i = 0; i < new_dim; i++) {
-        for (int j = 0; j < new_dim; j++) {
-            if (i < m->dimension && j < m->dimension) {
-                padded->elements[i][j] = m->elements[i][j];
-            } else {
-                padded->elements[i][j] = 0;
+Matrix* multiply_matrices(Matrix* m1, Matrix* m2) {
+    if (m1->dimension != m2->dimension)
+        return NULL;
+
+    Matrix* result = create_matrix(m1->dimension);
+    for (int i = 0; i < m1->dimension; i++) {
+        for (int j = 0; j < m1->dimension; j++) {
+            result->elements[i][j] = 0;
+            for (int k = 0; k < m1->dimension; k++) {
+                result->elements[i][j] += m1->elements[i][k] * m2->elements[k][j];
             }
         }
     }
-    return padded;
-}
-
-Matrix* unpad_matrix(Matrix* m, int original_dim) {
-    Matrix* unpadded = create_matrix(original_dim);
-    for (int i = 0; i < original_dim; i++) {
-        for (int j = 0; j < original_dim; j++) {
-            unpadded->elements[i][j] = m->elements[i][j];
-        }
-    }
-    return unpadded;
-}
-
-Matrix* strassen(Matrix* A, Matrix* B, int dim) {
-    if (dim == 1) {
-        Matrix* C = create_matrix(1);
-        C->elements[0][0] = A->elements[0][0] * B->elements[0][0];
-        return C;
-    }
-
-    int new_dim = dim / 2;
-    Matrix *a = create_matrix(new_dim);
-    Matrix *b = create_matrix(new_dim);
-    Matrix *c = create_matrix(new_dim);
-    Matrix *d = create_matrix(new_dim);
-    Matrix *e = create_matrix(new_dim);
-    Matrix *f = create_matrix(new_dim);
-    Matrix *g = create_matrix(new_dim);
-    Matrix *h = create_matrix(new_dim);
-
-    for (int i = 0; i < new_dim; i++) {
-        for (int j = 0; j < new_dim; j++) {
-            a->elements[i][j] = A->elements[i][j];
-            b->elements[i][j] = A->elements[i][j + new_dim];
-            c->elements[i][j] = A->elements[i + new_dim][j];
-            d->elements[i][j] = A->elements[i + new_dim][j + new_dim];
-            e->elements[i][j] = B->elements[i][j];
-            f->elements[i][j] = B->elements[i][j + new_dim];
-            g->elements[i][j] = B->elements[i + new_dim][j];
-            h->elements[i][j] = B->elements[i + new_dim][j + new_dim];
-        }
-    }
-
-    Matrix *p1 = strassen(a, subtract_matrices(f, h), new_dim);
-    Matrix *p2 = strassen(add_matrices(a, b), h, new_dim);
-    Matrix *p3 = strassen(add_matrices(c, d), e, new_dim);
-    Matrix *p4 = strassen(d, subtract_matrices(g, e), new_dim);
-    Matrix *p5 = strassen(add_matrices(a, d), add_matrices(e, h), new_dim);
-    Matrix *p6 = strassen(subtract_matrices(b, d), add_matrices(g, h), new_dim);
-    Matrix *p7 = strassen(subtract_matrices(a, c), add_matrices(e, f), new_dim);
-
-    Matrix *C11 = add_matrices(subtract_matrices(add_matrices(p5, p4), p2), p6);
-    Matrix *C12 = add_matrices(p1, p2);
-    Matrix *C21 = add_matrices(p3, p4);
-    Matrix *C22 = add_matrices(subtract_matrices(add_matrices(p5, p1), p3), p7);
-
-    Matrix *C = create_matrix(dim);
-    for (int i = 0; i < new_dim; i++) {
-        for (int j = 0; j < new_dim; j++) {
-            C->elements[i][j] = C11->elements[i][j];
-            C->elements[i][j + new_dim] = C12->elements[i][j];
-            C->elements[i + new_dim][j] = C21->elements[i][j];
-            C->elements[i + new_dim][j + new_dim] = C22->elements[i][j];
-        }
-    }
-
-    free_matrix(a); free_matrix(b); free_matrix(c); free_matrix(d);
-    free_matrix(e); free_matrix(f); free_matrix(g); free_matrix(h);
-    free_matrix(p1); free_matrix(p2); free_matrix(p3); free_matrix(p4);
-    free_matrix(p5); free_matrix(p6); free_matrix(p7);
-    free_matrix(C11); free_matrix(C12); free_matrix(C21); free_matrix(C22);
-
-    return C;
-}
-
-Matrix* multiply_matrices(Matrix* m1, Matrix* m2) {
-    int original_dim = m1->dimension;
-    int new_dim = pow(2, ceil(log2(original_dim)));
-
-    Matrix* padded_m1 = pad_matrix(m1, new_dim);
-    Matrix* padded_m2 = pad_matrix(m2, new_dim);
-
-    Matrix* padded_result = strassen(padded_m1, padded_m2, new_dim);
-
-    Matrix* result = unpad_matrix(padded_result, original_dim);
-
-    free_matrix(padded_m1);
-    free_matrix(padded_m2);
-    free_matrix(padded_result);
-
     return result;
 }
 
@@ -321,41 +261,84 @@ Matrix* scale_matrix(float c, Matrix* m) {
     return s;
 }
 
-float determinant_matrix(Matrix* m) {
+Matrix* scale_matrix_anisotropic(float x, float y, float z, Matrix* m) {
+    Matrix* s = create_matrix(m->dimension);
+    for (int i = 0; i < m->dimension; i++) {
+        for (int j = 0; j < m->dimension; j++) {
+            if (j == 0) s->elements[i][j] = m->elements[i][j] * x;
+            else if (j == 1) s->elements[i][j] = m->elements[i][j] * y;
+            else if (j == 2) s->elements[i][j] = m->elements[i][j] * z;
+            else if (j == 3) s->elements[i][j] = m->elements[i][j];
+        }
+    }
+    return s;
+}
 
+float determinant_matrix(Matrix* m) {
     if (m->dimension == 2)
         return (m->elements[0][0] * m->elements[1][1]) - (m->elements[0][1] * m->elements[1][0]);
 
     if (m->dimension == 1)
         return m->elements[0][0];
-
-    int factor = 1;
-    float determinant = 0;
-
-    for (int i = 0; i < m->dimension; i++) {
-        Matrix* submatrix = determinant_helper(m, 0, i);
-        determinant += factor * m->elements[0][i] * determinant_matrix(submatrix);
-        factor *= -1;
-        free_matrix(submatrix);
+    
+    if (m->dimension == 3) {
+        return m->elements[0][0] * (m->elements[1][1] * m->elements[2][2] - m->elements[1][2] * m->elements[2][1])
+             - m->elements[0][1] * (m->elements[1][0] * m->elements[2][2] - m->elements[1][2] * m->elements[2][0])
+             + m->elements[0][2] * (m->elements[1][0] * m->elements[2][1] - m->elements[1][1] * m->elements[2][0]);
     }
-    return determinant;
+
+    if (m->dimension == 4) {
+        return m->elements[0][0] * (
+                m->elements[1][1] * (m->elements[2][2] * m->elements[3][3] - m->elements[2][3] * m->elements[3][2])
+              - m->elements[1][2] * (m->elements[2][1] * m->elements[3][3] - m->elements[2][3] * m->elements[3][1])
+              + m->elements[1][3] * (m->elements[2][1] * m->elements[3][2] - m->elements[2][2] * m->elements[3][1])
+            )
+          - m->elements[0][1] * (
+                m->elements[1][0] * (m->elements[2][2] * m->elements[3][3] - m->elements[2][3] * m->elements[3][2])
+              - m->elements[1][2] * (m->elements[2][0] * m->elements[3][3] - m->elements[2][3] * m->elements[3][0])
+              + m->elements[1][3] * (m->elements[2][0] * m->elements[3][2] - m->elements[2][2] * m->elements[3][0])
+            )
+          + m->elements[0][2] * (
+                m->elements[1][0] * (m->elements[2][1] * m->elements[3][3] - m->elements[2][3] * m->elements[3][1])
+              - m->elements[1][1] * (m->elements[2][0] * m->elements[3][3] - m->elements[2][3] * m->elements[3][0])
+              + m->elements[1][3] * (m->elements[2][0] * m->elements[3][1] - m->elements[2][1] * m->elements[3][0])
+            )
+          - m->elements[0][3] * (
+                m->elements[1][0] * (m->elements[2][1] * m->elements[3][2] - m->elements[2][2] * m->elements[3][1])
+              - m->elements[1][1] * (m->elements[2][0] * m->elements[3][2] - m->elements[2][2] * m->elements[3][0])
+              + m->elements[1][2] * (m->elements[2][0] * m->elements[3][1] - m->elements[2][1] * m->elements[3][0])
+            );
+    }
+    return 0.0f;
 }
 
-Matrix* determinant_helper(Matrix* m, int exclude_row, int exclude_col) {
-    int new_dim = m->dimension - 1;
-    Matrix* sub = create_matrix(new_dim);
-    for (int i = 0, subi = 0; i < m->dimension; i++) {
-        if (i == exclude_row) continue;
-        for (int j = 0, subj = 0; j < m->dimension; j++) {
-            if (j == exclude_col) continue;
-            sub->elements[subi][subj] = m->elements[i][j];
-            subj++;
-        }
-        subi++;
-    }
-    return sub;
+Matrix* translate_matrix(Matrix* m, float x, float y, float z) {
+    Matrix* t = create_matrix(4);
+    for (int i = 0; i < 4; i++)
+        t->elements[i][i] = 1;
+
+    t->elements[3][0] = x;
+    t->elements[3][1] = y;
+    t->elements[3][2] = z;
+    return t;
 }
 
+Matrix* translate_matrix_in_place(Matrix* m, float x, float y, float z) {
+    return NULL;
+}
 
+Matrix* outer_product(Vector* v1, Vector* v2) {
+    Matrix* m = create_matrix(4);
+
+	for (int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+		    m->elements[i][j] = (i < 3 && j < 3) ? v1->elements[i] * v2->elements[j] : 0.f;
+
+    return m; 
+}
+
+Matrix* invert_matrix(Matrix* m) {
+	return NULL;
+}
 
 #endif
